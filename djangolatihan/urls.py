36e84ff9db86b4c0bcc20c0fa93fae28bb4ccbc1
Soekasir <soekasir@gmail.com>
2022-09-25 09:03:09 +0700

    opt: add static file

--- djangolatihan/urls.py
+++ djangolatihan/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls'))
+    path('products/', include('products.urls')),
 ]
+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
